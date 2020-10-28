1a28b7f9f4a4cf6421153bdcde7d8ed445db7d8c
Saumitra Paul <skpaul@gmail.com>
2020-10-29 00:01:24 +0600

    First commit

---
+++ DoubleClickCopy/DoubleClickCopy/frmMain.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DoubleClickCopy
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.chkToggleHandleDoubleClick = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chkToggleHandleDoubleClick
+            // 
+            this.chkToggleHandleDoubleClick.AutoSize = true;
+            this.chkToggleHandleDoubleClick.Checked = true;
+            this.chkToggleHandleDoubleClick.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.chkToggleHandleDoubleClick.Location = new System.Drawing.Point(149, 115);
+            this.chkToggleHandleDoubleClick.Name = "chkToggleHandleDoubleClick";
+            this.chkToggleHandleDoubleClick.Size = new System.Drawing.Size(123, 17);
+            this.chkToggleHandleDoubleClick.TabIndex = 0;
+            this.chkToggleHandleDoubleClick.Text = "Handle Double Click";
+            this.chkToggleHandleDoubleClick.UseVisualStyleBackColor = true;
+            this.chkToggleHandleDoubleClick.CheckedChanged += new System.EventHandler(this.chkToggleHandleDoubleClick_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Image = global::DoubleClickCopy.Properties.Resources.Mouse128;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 128);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(146, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Double Click: Copy Line.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(146, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(171, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Ctrl + Double Click: Copy Selection";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(146, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Ctrl + [+] : Copy";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(146, 72);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Ctrl + [-] : Paste";
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(325, 133);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.chkToggleHandleDoubleClick);
+            this.DoubleBuffered = true;
+            this.Name = "frmMain";
+            this.Text = "Double Click Copy";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox chkToggleHandleDoubleClick;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ DoubleClickCopy/DoubleClickCopy/frmMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using KMHooker;
+using KMHooker.WinApi;
+using System.Runtime.InteropServices;
+
+namespace DoubleClickCopy
+{
+    public partial class frmMain : Form
+    {
+        private readonly MouseHookListener m_MouseHookManager;
+        private readonly KeyboardHookListener m_KeyboardHookManager;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            this.Icon = Properties.Resources.ApplicationIcon128;
+            m_MouseHookManager = new MouseHookListener(new GlobalHooker());
+            m_MouseHookManager.Enabled = true;
+            m_MouseHookManager.MouseDoubleClick += MouseDoubleClicked;
+
+            m_KeyboardHookManager = new KeyboardHookListener(new GlobalHooker());
+            m_KeyboardHookManager.Enabled = true;
+            // m_KeyboardHookManager.KeyPress += HookManager_KeyPress;
+            m_KeyboardHookManager.KeyDown += HandleKeyDown;
+            
+            Hooker hook = new GlobalHooker();
+            m_KeyboardHookManager.Replace(hook);
+        }
+
+        bool controlKeyPressed = false;
+        private void HandleKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.LControlKey)
+            {
+                controlKeyPressed = true;
+            }
+
+            if (e.KeyCode == Keys.Add && e.Modifiers == Keys.Control)
+            {
+                e.Handled = true;
+                GetTextFromControlAtMousePosition();
+            }
+
+            if (e.KeyCode == Keys.Subtract && e.Modifiers == Keys.Control)
+            {
+                e.Handled = true;
+                SetTextFromControlAtMousePosition();
+            }
+        }
+
+
+        private void MouseDoubleClicked(object sender, MouseEventArgs e)
+        {
+            try
+            {
+                if (controlKeyPressed)
+                {
+                    if (e.Button == MouseButtons.Left)
+                    {
+                        GetTextFromControlAtMousePosition();
+                        controlKeyPressed = false;
+                    }
+
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        SetTextFromControlAtMousePosition();
+                        controlKeyPressed = false;
+                    }
+                }
+               
+
+            }
+            catch (Exception exp)
+            {
+                MessageBox.Show(exp.Message);
+            }
+        }
+
+        [DllImport("user32.dll")]
+        public static extern bool GetCursorPos(out Point pt);
+
+        [DllImport("user32.dll", EntryPoint = "WindowFromPoint", CharSet = CharSet.Auto, ExactSpelling = true)]
+        public static extern IntPtr WindowFromPoint(Point pt);
+
+        //Get the text of the control at the mouse position
+        private string GetTextFromControlAtMousePosition()
+        {
+            try
+            {
+                Point p;
+                if (GetCursorPos(out p))
+                {
+                    IntPtr ptr = WindowFromPoint(p);
+                    if (ptr != IntPtr.Zero)
+                    {
+                        SendCtrlC(ptr);
+                        //return GetText(ptr);
+                    }
+                }
+                return "";
+            }
+            catch (Exception exp)
+            {
+                return exp.Message;
+            }
+        }
+
+
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, uint dwExtraInfo);
+
+        [DllImport("User32.dll")]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private void SendCtrlC(IntPtr hWnd)
+        {
+            uint KEYEVENTF_KEYUP = 2;
+            byte VK_CONTROL = 0x11;
+            SetForegroundWindow(hWnd);
+            keybd_event(VK_CONTROL, 0, 0, 0);
+            keybd_event(0x43, 0, 0, 0); //Send the C key (43 is "C")
+            keybd_event(0x43, 0, KEYEVENTF_KEYUP, 0);
+            keybd_event(VK_CONTROL, 0, KEYEVENTF_KEYUP, 0);// 'Left Control Up
+        }
+
+        //Set the text of the control at the mouse position
+        private string SetTextFromControlAtMousePosition()
+        {
+            try
+            {
+                Point p;
+                if (GetCursorPos(out p))
+                {
+                    IntPtr ptr = WindowFromPoint(p);
+                    if (ptr != IntPtr.Zero)
+                    {
+                        SendCtrlV(ptr);
+                    }
+                }
+                return "";
+            }
+            catch (Exception exp)
+            {
+                return exp.Message;
+            }
+        }
+
+        [DllImport("user32.dll")]
+        static extern short MapVirtualKey(int wCode, int wMapType);
+        private void SendCtrlV(IntPtr hWnd)
+        {
+            keybd_event((int)Keys.ControlKey, (byte)MapVirtualKey((int)Keys.ControlKey, 0), 0, 0); // Control Down
+
+            keybd_event((int)Keys.V, (byte)MapVirtualKey((int)Keys.V, 0), 0, 0); // V Down
+            keybd_event((int)Keys.V, (byte)MapVirtualKey((int)Keys.V, 0), 2, 0); // V Up
+
+            keybd_event((int)Keys.ControlKey, (byte)MapVirtualKey((int)Keys.ControlKey, 0), 2, 0); // Control Up
+        }
+
+        private void chkToggleHandleDoubleClick_CheckedChanged(object sender, EventArgs e)
+        {
+            CheckBox chk = sender as CheckBox;
+            if (chk.CheckState==CheckState.Checked)
+            {
+                m_MouseHookManager.MouseDoubleClick += MouseDoubleClicked;
+            }
+            else
+            {
+                m_MouseHookManager.MouseDoubleClick -= MouseDoubleClicked;
+            }
+        }
+    }
+}
